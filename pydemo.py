397796a0e5640bb2936d0748c5792ce9418e24fc
sambath-kumar <64127985+sambath-kumar@users.noreply.github.com>
2020-09-02 11:24:12 +0530

    compared two values added
    Equal, Greater and Lesser

--- pydemo.py
+++ pydemo.py
@@ -5,4 +5,14 @@ print("Thank you all!")
 while i==10:
     print("Thanks for your patience")
     
+A = int(input("Enter a first value:"))
+B = int(input("Enter a second value:"))
+
+if A == B:
+    print("Both values are same")
+elif a>b:
+    print("A is large")
+else:
+    print("B is large")
+    
   
